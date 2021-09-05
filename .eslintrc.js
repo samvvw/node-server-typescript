c501fce346fbaaa79dccfcacb514417f11c06468
Sam Villegas <svillegaswarre00@mylangara.ca>
2021-09-04 21:11:12 -0700

    test renamed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    "node": true,
-    "commonjs": true
-},
+    node: true,
+    commonjs: true,
+  },
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier'
+    'prettier',
   ],
-
-
-};
+}
